7ce61bc13a6ab8547dc33ad3a01633bfb564a557
icadir <ismailcadir@gmail.com>
2021-02-14 09:38:20 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // iş Kodları
+            // yetkisi var mı?
+           return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    // çıplak class kalmasın
+   public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
