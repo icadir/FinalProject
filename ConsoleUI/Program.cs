728311c4e1c09a3b0f8dc35bd03cdb229c55a7c8
icadir <ismailcadir@gmail.com>
2021-02-15 22:07:19 +0300

    Refactor-2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,14 +11,26 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             // test test
+            //ProductTest();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var item in productManager.GetAll())
             {
                 Console.WriteLine(item.ProductName);
             }
-
-            Console.ReadKey();
         }
     }
 }
