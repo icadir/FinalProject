eb721922715d0de5ecd768759d3ae060574503e7
icadir <ismailcadir@gmail.com>
2021-02-15 21:10:13 +0300

    Refactoring

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using Entities.Abstract;
+using Core.Entities;
 
-namespace DataAccess.Abstract
+
+namespace Core.DataAccess
 {
     // Generic constraint( generic kısıt)
     // class : referans tip olabilir.
     // IEntity : I entity olabilir veya Ientityden implement edilmiş bir referans tip olabilir
     //new :  newlenebilir bir nesne olmalı
-    public interface IEntityRepository<T> where T : class, IEntity, new ()
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
